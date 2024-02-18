ee987ca639e91050642b040317eef59fef81f763
TheDeadCell <67496505+TheDeadCell@users.noreply.github.com>
2024-02-18 21:52:00 +0000

    Add Nala and Zoxide to script

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 #Made By Arthur Schieszer For Cyberpatriot Team Neofetch
 #
 #Installs necesecary programs and updates the system
-apt -qq --yes install ufw net-tools gufw clamav neofetch htop auditd fail2ban ranger micro tldr
+apt -qq --yes install ufw net-tools gufw clamav neofetch htop auditd fail2ban ranger micro tldr nala zoxide
 apt -qq --yes update
 apt -qq --yes purge ophcrack nginx wireshark npcap nc ettercap ettercap-graphical
 echo "Would you like to install system updates now? [y/N]"
